6a0af7fe80ef958ab0b582fae2daa1f4e68e3d4b
Gabriel Ichim <ichimgabriel75@gmail.com>
2020-09-12 23:14:45 +0100

    add models

---
+++ FNUAPI/Models/UserRolePairModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FNUAPI.Models
+{
+    public class UserRolePairModel
+    {
+        public string Id { get; set; }
+        public string RoleName { get; set; }
+    }
+}
--- FNUAPI/Program.cs
+++ FNUAPI/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace FNU
+namespace FNUAPI
 {
     public class Program
     {
